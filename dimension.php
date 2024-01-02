0e9eba3c63192e71545e2cd27659d30d024fca1a
Hugh Barnes <dev@hubns.com>
2024-01-02 21:35:26 +1300

    Fix form and field and button markup for theme, extending Form plugin templates

--- dimension.php
+++ dimension.php
@@ -5,5 +5,22 @@ use Grav\Common\Theme;
 
 class Dimension extends Theme
 {
-    // Access plugin events in this class
+
+    /**
+     * @return array
+     *
+     */
+    public static function getSubscribedEvents(): array {
+        return [
+            'onTwigLoader' => ['registerFormTemplateAlias', 10],
+        ];
+    }
+
+    public function registerFormTemplateAlias() {
+        if($this->grav['config']->get('plugins.form.enabled')) {
+            $form_plugin_path = $this->grav['locator']->findResource('plugins://form');
+            $form_path = $form_plugin_path . DIRECTORY_SEPARATOR . 'templates';
+            $this->grav['twig']->addPath($form_path, 'formplugin');
+        }
+    }
 }
